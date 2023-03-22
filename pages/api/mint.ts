35d30e9ee4ea15a8a895cacba7d758c144c4863a
Danny Mulvihill <danny@paella.dev>
2023-03-21 21:24:45 -0500

    ready for initial launch

---
+++ pages/api/mint.ts
@@ -0,0 +1,43 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import fetch from 'node-fetch';
+import crypto from 'crypto';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+
+  try {
+    const nftId = crypto.createHash('sha1').update(req.body.email).digest('hex');
+    console.log('req.body:', req.body);
+    console.log('email:', req.body.email);
+    console.log('nftId:', nftId);
+    const recipientString = `email:${req.body.email}:polygon`;
+
+    const url = `${process.env.API_URL}/${nftId}`;
+
+    const response = await fetch(url, {
+      method: 'PUT',
+      headers: {
+        "content-type": "application/json",
+        "x-client-secret": process.env.CLIENT_SECRET as string,
+        "x-project-id": process.env.PROJECT_ID as string
+      },
+      body: JSON.stringify({
+        recipient: recipientString,
+        metadata: {
+          name: 'Crossmint Minting API Demo',
+          image: 'https://bafkreievg3akxgi2arfi6njrmkyy55vga5tfnav5nx2btcew2z7hbgpbyy.ipfs.nftstorage.link/',
+          description: 'Demo collection showcasing how easy it is to MINT NFTs with API'
+        },
+        reuploadLinkedFiles: false
+      })
+    });
+    
+    return res.status(response.status).json(await response.json());
+  }
+  catch (error: any) {
+    res.json(error);
+    res.status(405).end();
+  }
+}
---
+++ pages/api/status.ts
@@ -0,0 +1,31 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import fetch from 'node-fetch';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+
+  try {
+    console.log('req.query:', req.query);
+    console.log('email:', req.query.id);
+
+    const nftId = req.query.id;
+    const url = `${process.env.API_URL}/${nftId}`;
+
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        "content-type": "application/json",
+        "x-client-secret": process.env.CLIENT_SECRET as string,
+        "x-project-id": process.env.PROJECT_ID as string
+      }
+    });
+    
+    return res.status(response.status).json(await response.json());
+  }
+  catch (error: any) {
+    res.json(error);
+    res.status(405).end();
+  }
+}
